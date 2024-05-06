7d14449171be98da5b53fb93c78adeb796c70f87
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2024-05-06 07:35:34 -0300

    update

--- setup.sh
+++ setup.sh
@@ -15,16 +15,16 @@ xdg-mime default Thunar.desktop inode/directory
 echo "/usr/bin/pipewire & /usr/bin/pipewire-pulse & usr/bin/pipewire-media-session" > ~/.xinitrc
 
 mkdir -p ~/.local/share/fonts/ttf/GohuFont
-
+ 
 cp ./GohuFont.ttf ~/.local/share/fonts/ttf/GohuFont
 cp ./wallpaper.jpg ~/Pictures/wallpaper.jpg
 
-cp -r ./ly /etc/ly
-cp -r ./wofi ~/.config/wofi
-cp -r ./hypr ~/.config/hypr
-cp -r ./nvim ~/.config/nvim
-cp -r ./foot ~/.config/foot
-cp -r ./HybridBar ~/.config/HybridBar
+sudo cp -T -f -r ./ly /etc/ly
+cp -T -f -r ./wofi ~/.config/wofi
+cp -T -f -r ./hypr ~/.config/hypr
+cp -T -f -r ./nvim ~/.config/nvim
+cp -T -f -r ./foot ~/.config/foot
+cp -T -f -r ./HybridBar ~/.config/HybridBar
 
 sudo pacman -R vim --noconfirm
 sudo pacman -R nano --noconfirm
