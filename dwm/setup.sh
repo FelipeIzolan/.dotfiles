b9ae55e0ad965c8408ee3bb4e513cd50aaa9a04b
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2024-06-17 00:28:57 -0300

    fix

--- dwm/setup.sh
+++ dwm/setup.sh
@@ -8,16 +8,16 @@ _patch() {
   patch -l -i $1 $2
 }
 
-sudo pacman -S xdg-user-dirs-update --noconfirm
-sudo pacman -S xsetroot --noconfirm
-sudo pacman -S git, wget --noconfirm
+sudo pacman -S xdg-user-dirs --noconfirm
+sudo pacman -S xorg-xsetroot --noconfirm
+sudo pacman -S git wget --noconfirm
 sudo pacman -S neovim --noconfirm
 sudo pacman -S xwallpaper --noconfirm
 sudo pacman -S nemo --noconfirm
 sudo pacman -S scrot xclip --noconfirm
 
 sudo pacman -S xorg-server xorg-xinit --noconfirm
-sudo pacman -S base-devel libx11 libxft libxinerama
+sudo pacman -S base-devel libx11 libxft libxinerama --noconfirm
 
 xdg-user-dirs-update
 xdg-mime default Nemo.desktop inode/directory
@@ -63,4 +63,4 @@ printf "xwallpaper --center ~/wallpaper.jpg & setxkbmap br & ~/statusbar.sh & ex
 
 chmod +x ~/statusbar.sh
 
-sudo pacman -R wget xdg-user-dirs-update --noconfirm
+sudo pacman -R wget xdg-user-dirs --noconfirm
